9519d43d6b33f8e8205ac4a23894e737455da63a
Chandra Sivaraman <cs31415@yahoo.com>
2020-09-24 21:33:30 -0700

    Added heap, dijkstra

---
+++ Collections/src/Dijkstra.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    /// <summary>
+    /// Find shortest path from a single source vertex to all
+    /// reachable vertices in a directed graph
+    /// </summary>
+    public class Dijkstra
+    {
+        /// <summary>
+        /// Given graph G, determine shortest paths to all reachable vertices from source s
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="s"></param>
+        public void ShortestPath(Graph g, Vertex s)
+        {
+            if (g == null)
+            {
+                throw new NullReferenceException("invalid graph");
+            }
+            if (s == null)
+            {
+                throw new NullReferenceException("invalid source vertex");
+            }
+
+            Initialize(g, s);
+            
+            var S = new List<Vertex>(); // Contains vertices whose shortest paths have been determined
+
+            // priority queue (min-heap) on Vertex.Distance
+            var Q = new Heap<Vertex>(true); 
+            Q.BuildHeap(g.Vertices.Values.ToList());
+            
+            while (Q.HeapSize > 0)
+            {
+                var u = Q.ExtractHead();
+                S.Add(u);
+                u.Edges.ToList().ForEach(k =>
+                {
+                    if (Relax(u, k.Dest))
+                        Q.Update(k.Dest); // Refresh priority Q if priority was updated
+                });
+            }
+        }
+
+        private void Initialize(Graph g, Vertex s)
+        {
+            g.Vertices.ToList().ForEach(v =>
+            {
+                v.Value.Distance = int.MaxValue;
+                v.Value.Predecessor = null;
+            });
+            s.Distance = 0;
+        }
+
+        /// <summary>
+        /// Weight of the edge from u to v
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        private int Weight(Vertex u, Vertex v)
+        {
+            var edge = u.Edges.FirstOrDefault(e => e.Dest.Key == v.Key);
+            return edge?.Weight ?? int.MaxValue;
+        }
+
+        /// <summary>
+        /// Adjust v's distance from s if a shorter path exists through u
+        /// </summary>
+        /// <param name="u"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        private bool Relax(Vertex u, Vertex v)
+        {
+            int w = Weight(u, v);
+            if (v.Distance > u.Distance + w)
+            {
+                v.Distance = u.Distance + w;
+                v.Predecessor = u;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Collections/src/Graph.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    /// <summary>
+    /// This class can be used to represent a directed or undirected graph.
+    /// It uses adjacency list representation.
+    /// </summary>
+    public class Graph
+    {
+        public Dictionary<string, Vertex> Vertices { get; private set; } = new Dictionary<string, Vertex>();
+
+        public Vertex AddVertex(string key)
+        {
+            if (!Vertices.ContainsKey(key))
+            {
+                var v = new Vertex(key);
+                Vertices.Add(key, v);
+                return v;
+            }
+
+            return Vertices[key];
+        }
+
+        public Vertex GetVertex(string key)
+        {
+            return Vertices[key];
+        }
+
+        /// <summary>
+        /// If there are missing back connection(s) then the graph is directed
+        /// </summary>
+        /// <returns></returns>
+        public bool IsDirected()
+        {
+            return !Vertices.ToList().All(p => HasBackConnection(p.Value));
+        }
+
+        public void Write(Action<string> fWrite)
+        {
+            var type = IsDirected() ? "directed" : "undirected";
+            fWrite($"graph is {type}");
+            Vertices.ToList().ForEach(p => p.Value.Write(fWrite));
+        }
+
+        private bool HasBackConnection(Vertex v)
+        {
+            return v.Edges.ToList().Any(c => c.Dest.Edges.ToList().Any(b => b.Dest.Key == v.Key));
+        }
+    }
+
+    /// <summary>
+    /// Represents a Vertex in the graph
+    /// </summary>
+    public class Vertex : IComparable<Vertex>, IEquatable<Vertex>
+    {
+        public string Key { get; set; }
+        public int Distance { get; set; } // distance from source
+        public Vertex Predecessor { get; set; }
+        public IList<Edge> Edges { get; private set; } = new List<Edge>();
+
+        public Vertex(string key)
+        {
+            Key = key;
+        }
+
+        public int CompareTo(Vertex other)
+        {
+            if (other == null)
+                return 1;
+
+            return Distance.CompareTo(other.Distance);
+        }
+
+        public void AddEdge(Vertex v, int w)
+        {
+            if (!Edges.Any(a => a.Dest.Key == v.Key))
+            {
+                Edges.Add(new Edge(this, v, w));
+            }
+        }
+
+        public void Write(Action<string> fWrite)
+        {
+            fWrite($"{Key} : ");
+            Edges.ToList().ForEach(v => fWrite($"{v.Dest.Key} > "));
+            fWrite($"nil\n");
+        }
+
+        public bool Equals(Vertex other)
+        {
+            return Key == other?.Key;
+        }
+    }
+
+    public class Edge
+    {
+        public Vertex Source { get; }
+        public Vertex Dest { get; }
+        public int Weight { get; set; }
+
+        public Edge(Vertex source, Vertex dest, int weight)
+        {
+            Source = source;
+            Dest = dest;
+            Weight = weight;
+        }
+    }
+}
